1216363a32647cdbb14a95fa1572c40accdbba5d
Spyros Livathinos <livathinos.spyros@gmail.com>
2015-10-15 15:57:56 +0200

    Add GithubEvents module

---
+++ lib/repoflow/github_events.ex
@@ -0,0 +1,21 @@
+defmodule Repoflow.GithubEvents do
+  @user_agent [ {"User-agent", "Elixir spyros@zendesk.com" } ]
+
+  def fetch(user, project) do
+    events_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def events_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/events"
+  end
+
+  def handle_response(%{status_code: 200, body: body}) do
+    { :ok, body }
+  end
+
+  def handle_reponse(%{status_code: __, body: body}) do
+    { :error, body }
+  end
+end
