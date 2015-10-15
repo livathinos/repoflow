1b5172dae396cf237188a9cb88577d1fb7d21878
Spyros Livathinos <livathinos.spyros@gmail.com>
2015-10-15 15:51:57 +0200

    Add CLI class

--- lib/repoflow/cli.ex
+++ lib/repoflow/cli.ex
@@ -1,2 +1,38 @@
 defmodule Repoflow.CLI do
+  @default_count 4
+
+  def run(argv) do
+    argv
+      |> parse_args
+      |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(
+      argv,
+      switches: [ help: :boolean],
+      aliases: [h: :help]
+    )
+
+    case parse do
+      { [ help: true ], _, _ }
+        -> :help
+      { _, [ user, project, count ], _ }
+        -> { user, project, String.to_integer(count) }
+      { _, [ user, project ], _ }
+        -> { user, project, @default_count }
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: flow <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Repoflow.GithubEvents.fetch(user, project)
+  end
 end
---
+++ test/cli_test.exs
@@ -0,0 +1,18 @@
+defmodule CliTest do
+  use ExUnit.Case
+
+  import Repoflow.CLI, only: [ parse_args: 1 ]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "shows a default count if two values are given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+end
