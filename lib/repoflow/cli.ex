2c6a92f9b42262996cdad3572f081c24620431c4
Spyros Livathinos <livathinos.spyros@gmail.com>
2015-10-15 16:46:24 +0200

    Sort results by ascending order

--- lib/repoflow/cli.ex
+++ lib/repoflow/cli.ex
@@ -36,6 +36,7 @@ defmodule Repoflow.CLI do
     Repoflow.GithubEvents.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
   end
 
   def decode_response({ :ok, body }), do: body
@@ -48,5 +49,10 @@ defmodule Repoflow.CLI do
 
   def convert_to_list_of_hashdicts(list) do list
     |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,7 +1,11 @@
 defmodule CliTest do
   use ExUnit.Case
 
-  import Repoflow.CLI, only: [ parse_args: 1 ]
+  import Repoflow.CLI, only: [
+    parse_args: 1,
+    sort_into_ascending_order: 1,
+    convert_to_list_of_hashdicts: 1
+  ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -14,5 +18,17 @@ defmodule CliTest do
 
   test "shows a default count if two values are given" do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "sort events by ascending order" do
+    result = sort_into_ascending_order(created_at_list(["c", "a", "b"]))
+    events = for event <- result, do: event["created_at"]
+    assert events == ~w{a b c}
+  end
+
+  defp created_at_list(values) do
+    data = for value <- values,
+      do: [{"created_at", value}, {"other_data", "xxx"}]
+    convert_to_list_of_hashdicts data
   end
 end
